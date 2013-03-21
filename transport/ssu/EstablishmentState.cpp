a260d40f5c42bb49b80556125706ae75200ba8ca
orion <centromere@mail.i2p>
2013-03-21 17:29:29 -0400

    Developed the ES a bit.

--- transport/ssu/EstablishmentState.cpp
+++ transport/ssu/EstablishmentState.cpp
@@ -1,15 +1,31 @@
 #include "EstablishmentState.h"
 
+#include <botan/auto_rng.h>
+
 namespace i2pcpp {
 	namespace SSU {
-		EstablishmentState::EstablishmentState(Endpoint const &ep, SessionKey const &sessionKey) :
-			m_direction(EstablishmentState::INBOUND)
+		EstablishmentState::EstablishmentState(Botan::DL_Group const &params, Botan::DSA_PrivateKey const &dsaKey, Endpoint const &ep, SessionKey const &sk) :
+			m_direction(EstablishmentState::INBOUND),
+			m_theirEndpoint(ep)
 		{
+			Botan::AutoSeeded_RNG rng;
+			Botan::DL_Group group("modp/ietf/2048");
+
+			m_dhKey = new Botan::DH_PrivateKey(rng, group);
 		}
 
-		EstablishmentState::EstablishmentState(Endpoint const &ep, SessionKey const &sessionKey, RouterIdentity const &ri) :
-			m_direction(EstablishmentState::INBOUND)
+		EstablishmentState::EstablishmentState(Botan::DL_Group const &params, Botan::DSA_PrivateKey const &dsaKey, Endpoint const &ep, SessionKey const &sk, RouterIdentity const &ri) :
+			m_direction(EstablishmentState::OUTBOUND),
+			m_theirIdentity(ri),
+			m_theirEndpoint(ep)
 		{
+			EstablishmentState(params, dsaKey, ep, sk);
+		}
+
+		EstablishmentState::~EstablishmentState()
+		{
+			if(m_dhKey)
+				delete m_dhKey;
 		}
 
 		EstablishmentState::Direction EstablishmentState::getDirection() const
--- transport/ssu/EstablishmentState.h
+++ transport/ssu/EstablishmentState.h
@@ -4,6 +4,9 @@
 #include <memory>
 #include <mutex>
 
+#include <botan/dh.h>
+#include <botan/dsa.h>
+
 #include "../../datatypes/Endpoint.h"
 #include "../../datatypes/SessionKey.h"
 #include "../../datatypes/RouterIdentity.h"
@@ -12,8 +15,10 @@ namespace i2pcpp {
 	namespace SSU {
 		class EstablishmentState {
 			public:
-				EstablishmentState(Endpoint const &ep, SessionKey const &sessionKey);
-				EstablishmentState(Endpoint const &ep, SessionKey const &sessionKey, RouterIdentity const &ri);
+				EstablishmentState(Botan::DL_Group const &params, Botan::DSA_PrivateKey const &dsaKey, Endpoint const &ep, SessionKey const &sk);
+				EstablishmentState(Botan::DL_Group const &params, Botan::DSA_PrivateKey const &dsaKey, Endpoint const &ep, SessionKey const &sk, RouterIdentity const &ri);
+				EstablishmentState(EstablishmentState const &state) = delete;
+				~EstablishmentState();
 
 				enum Direction {
 					INBOUND,
@@ -24,6 +29,10 @@ namespace i2pcpp {
 
 			private:
 				Direction m_direction;
+				Botan::DH_PrivateKey *m_dhKey;
+
+				RouterIdentity m_theirIdentity;
+				Endpoint m_theirEndpoint;
 		};
 
 		typedef std::shared_ptr<EstablishmentState> EstablishmentStatePtr;
