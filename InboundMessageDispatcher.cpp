4928ea1517d03fd435206525720ce855691af5c3
orion <centromere@mail.i2p>
2013-03-30 17:16:35 -0400

    Send my RouterInfo too.

--- InboundMessageDispatcher.cpp
+++ InboundMessageDispatcher.cpp
@@ -3,8 +3,10 @@
 #include <boost/bind.hpp>
 
 #include <botan/auto_rng.h>
+#include <botan/zlib.h>
 
 #include "i2np/DeliveryStatus.h"
+#include "i2np/DatabaseStore.h"
 
 #include "RouterContext.h"
 
@@ -68,6 +70,34 @@ namespace i2pcpp {
 
 			I2NP::MessagePtr m(new I2NP::DeliveryStatus(msgId, Date(2)));
 			m_ctx.getOutMsgDisp().sendMessage(rh, m);
+
+			Mapping am;
+			am.setValue("caps", "BC");
+			am.setValue("host", m_ctx.getDatabase().getConfigValue("ssu_external_ip"));
+			am.setValue("key", m_ctx.getIdentity().getHashEncoded());
+			am.setValue("port", m_ctx.getDatabase().getConfigValue("ssu_external_port"));
+			RouterAddress a(5, Date(0), "SSU", am);
+
+			Mapping rm;
+			rm.setValue("coreVersion", "0.9.5");
+			rm.setValue("netId", "2");
+			rm.setValue("router.version", "0.9.5");
+			rm.setValue("stat_uptime", "90m");
+			RouterInfo myInfo(m_ctx.getIdentity(), Date(), rm);
+			myInfo.addAddress(a);
+			myInfo.sign(m_ctx.getSigningKey());
+
+			Botan::Pipe gzPipe(new Botan::Zlib_Compression);
+			gzPipe.start_msg();
+			gzPipe.write(myInfo.serialize());
+			gzPipe.end_msg();
+
+			unsigned int size = gzPipe.remaining();
+			ByteArray gzInfoBytes(size);
+			gzPipe.read(gzInfoBytes.data(), size);
+
+			auto mydsm = std::make_shared<I2NP::DatabaseStore>(myInfo.getIdentity().getHash(), I2NP::DatabaseStore::DataType::ROUTER_INFO, 0, gzInfoBytes);
+			m_ctx.getOutMsgDisp().sendMessage(rh, mydsm);
 		}
 	}
 }
