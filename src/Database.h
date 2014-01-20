93725bba561657863619a3a274d5f35a4e053896
orion <centromere@mail.i2p>
2014-01-19 23:32:26 -0500

    Fixed Database binding code which invokes undefined behavior.

--- src/Database.h
+++ src/Database.h
@@ -102,18 +102,29 @@ namespace i2pcpp {
 
         private:
             class statement_guard {
-                struct pass {
-                    template<typename ...T> pass(T...) {}
-                };
-
                 boost::shared_ptr<sqlite::detail::basic_statement> m_statement;
+
+                void bind() {}
+
+                template<typename T>
+                void bind(T&& t)
+                {
+                    *m_statement << t;
+                }
+
+                template<typename T, typename ...U>
+                void bind(T&& t, U&& ...u)
+                {
+                    *m_statement << t;
+                    bind(u...);
+                }
 
                 public:
                 template<typename ...Params>
                 statement_guard(boost::shared_ptr<sqlite::detail::basic_statement> s,
                         Params&&... p) : m_statement(s)
                 {
-                    pass{(*m_statement << p, 0) ...};
+                    bind(std::forward<Params>(p)...);
                 }
 
                 ~statement_guard()
