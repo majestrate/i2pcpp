abef400bd54f7b1209dc020fa23e4cf43f3d56d3
orion <centromere@mail.i2p>
2014-01-21 13:04:19 -0500

    Expanded API.

--- include/i2pcpp/router.hpp
+++ include/i2pcpp/router.hpp
@@ -1,5 +1,5 @@
-#ifndef I2PROUTER_HPP
-#define I2PROUTER_HPP
+#ifndef I2PAPI_HPP
+#define I2PAPI_HPP
 
 #define I2PAPI_VERSION 0
 
@@ -7,7 +7,12 @@
 extern "C" {
 #endif
 
+void i2p_init();
 void *i2p_router_new(char *dbFile);
+void i2p_router_free(void *router);
+void i2p_db_create(char *dbFile);
+void i2p_db_config_set(void *router, char *name, char *value);
+char *i2p_db_config_get(void *router, char *name);
 
 #ifdef __cplusplus
 }
---
+++ src/api/router.cpp
@@ -0,0 +1,46 @@
+#include <i2pcpp/router.hpp>
+
+#include <string>
+
+#include <botan/botan.h>
+
+#include "Router.h"
+
+using namespace i2pcpp;
+
+void i2p_init()
+{
+    Log::initialize();
+    Botan::LibraryInitializer init("thread_safe=true");
+}
+
+void *i2p_router_new(char *dbFile)
+{
+    Router *r = new (std::nothrow) Router(std::string(dbFile));
+
+    return r;
+}
+
+void i2p_router_free(void *router)
+{
+    delete (Router *)router;
+}
+
+void i2p_db_create(char *dbFile)
+{
+    Database::createDb(std::string(dbFile));
+}
+
+void i2p_db_config_set(void *router, char *name, char *value)
+{
+    ((Router *)router)->setConfigValue(std::string(name), std::string(value));
+}
+
+char *i2p_db_config_get(void *router, char *name)
+{
+    std::string result = ((Router *)router)->getConfigValue(std::string(name));
+    char *buf = new char[result.length() + 1];
+    std::memcpy(buf, result.c_str(), sizeof(*buf));
+
+    return buf;
+}
